f9808f7441dd7250ba4ed503d9c370fce2fbf620
kind-sailor-1643 <bhandarireeva@gmail.com>
2019-06-27 09:26:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,21 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+@board=[" "," "," "," "," "," "," "," "," "]
+
+def display_board
+  
+  puts "#{@board[0]}  |#{@board[1]}  |#{@board[2]}  "
+  puts "-----------"
+  puts "#{@board[3]}  |#{@board[4]}  |#{@board[5]}  "
+  puts "-----------"
+  puts "#{@board[6]}  |#{@board[7]}  |#{@board[8]}  "
+ 
+end
+
+display_board
+
+puts "Enter the position"
+position=gets.to_i
+index=position-1
+puts @board[index]="X"
+display_board
